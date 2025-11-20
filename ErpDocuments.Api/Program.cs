dfa78a9b17c2b49b64cace0678b9d6352c3c9424
Andres Camilo Solano Pantoja <andresolano.12.651@gmail.com>
2025-11-20 08:04:50 -0500

    Infrastructure setup: EF Core 8, DbContext, SQL Server connection and DI

--- ErpDocuments.Api/Program.cs
+++ ErpDocuments.Api/Program.cs
@@ -1,9 +1,18 @@
+﻿using ErpDocuments.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// ✅ EF Core - DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
---
+++ ErpDocuments.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ErpDocuments.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
